7a4fc7ed70194b7dfca45c0eb8a563f1136a032d
wintery7 <apddl5@naver.com>
2024-01-24 16:48:05 +0900

    0.0.0

---
+++ src/main/java/com/springboot/government_data_project/GovernmentDataProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.government_data_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernmentDataProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GovernmentDataProjectApplication.class, args);
+    }
+
+}
