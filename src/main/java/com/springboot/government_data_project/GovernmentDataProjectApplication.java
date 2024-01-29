5d772260c8102dea49eacfe67b239c3e7d2b3388
realcold0 <realcold0@gmail.com>
2024-01-29 13:32:31 +0900

    chore : git ignore 파일 수정 및 application.properties db 접속 수정

--- src/main/java/com/springboot/government_data_project/GovernmentDataProjectApplication.java
+++ src/main/java/com/springboot/government_data_project/GovernmentDataProjectApplication.java
@@ -2,10 +2,10 @@ package com.springboot.government_data_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class GovernmentDataProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GovernmentDataProjectApplication.class, args);
     }
