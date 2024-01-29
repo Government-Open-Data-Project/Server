4c8e3afd774db4a48f043b33e056c7ede93564f2
realcold0 <realcold0@gmail.com>
2024-01-29 13:33:59 +0900

    feat : spring security 초기 세팅

---
+++ src/main/java/com/springboot/government_data_project/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.springboot.government_data_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http)throws Exception{
+
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().permitAll() //모든 링크의 요청 승인(개발 동안만)
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
