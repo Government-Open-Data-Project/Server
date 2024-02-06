982ff0999cb589406c801a1953eb86b09f300f6d
wintery7 <apddl5@naver.com>
2024-02-06 19:41:56 +0900

    fix : 투표 수정

--- src/main/java/com/springboot/government_data_project/repository/LawRepository.java
+++ src/main/java/com/springboot/government_data_project/repository/LawRepository.java
@@ -1,10 +1,13 @@
 package com.springboot.government_data_project.repository;
 
-import com.springboot.government_data_project.dto.law.LawResponseDTO;
+import com.springboot.government_data_project.domain.Law;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface LawRepository extends JpaRepository<LawResponseDTO, Long> {
+import java.util.Optional;
 
+@Repository
+public interface LawRepository extends JpaRepository<Law, Long> {
+
+    Optional<Law> findByBillId(Long billId);
 }
